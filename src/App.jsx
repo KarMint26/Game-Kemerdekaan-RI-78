b76f99da2473890669b0a65fe247e01bbc97c782
Karel <kareltrisnanto26@gmail.com>
2023-08-09 17:01:20 +0700

    edit: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <>
+      <div className="hello">Hello World</div>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/game-kemerdekaan-ri" element={<Game />} />
