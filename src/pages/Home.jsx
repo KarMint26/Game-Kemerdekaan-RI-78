dec9446b49cc2e5368277b7ceccd8405849768f2
Karel <kareltrisnanto26@gmail.com>
2023-08-09 15:20:13 +0700

    edit: home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
+      <div className="hello">Hello World</div>
       <div className='hero min-h-screen bg-[url(/src/assets/Hero.jpg)] bg-cover bg-no-repeat'>
         <div className='hero-overlay bg-opacity-60'></div>
         <div className='hero-content text-center text-neutral-content'>
