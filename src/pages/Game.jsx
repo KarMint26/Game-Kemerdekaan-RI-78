b78822f9e635b5e2ed3a44da3800bea1fbe88a26
Karel <kareltrisnanto26@gmail.com>
2023-08-14 19:48:58 +0700

    edit: Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -8,7 +8,7 @@ const Game = () => {
         <div className='hero-overlay bg-opacity-60'></div>
         <div className='hero-content text-center text-neutral-content'>
           <div className='max-w-md'>
-            <h1 className='mb-5 text-3xl sm:text-5xl font-bold'>Daftar Game</h1>
+            <h1 className='mb-5 text-3xl sm:text-5xl font-bold'>Game Balap Karung</h1>
           </div>
         </div>
       </div>
